8df3a0b213ffc663fdcada3ba422a07e4890c479
Philipp <54840988+PhilippStraub@users.noreply.github.com>
2019-12-05 15:23:08 +0100

    Alle Methoden fertig. Grob getestet

--- TicTacToe/src/com/company/TicTacToe.java
+++ TicTacToe/src/com/company/TicTacToe.java
@@ -39,63 +39,95 @@ public class TicTacToe {
 	}
 	
 	public boolean isGameWon() {
-		if(this.winner == null || this.winner == Player.Unknown) {
-			return false;
+		boolean result = false;
+		if(this.winner == null) {
+			//Spiel läuft noch
+			
+			
+			//Überprüfung ob ein Spieler gewonnen hat -> falls true -> result = false;
+			
+			//Überprüfung ob Reihe gewonnen
+			for(int i=0; i<this.board.length; i++) {
+				
+				if(this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2] && this.board[i][0] != Player.Unknown) {
+					result = true;
+					this.winner = currentPlayer;
+					
+					return result;
+					
+				} else {
+					result = false;
+				}
+				
+			}
+			
+			//Überprüfung ob Spalte gewonnen
+			for(int i=0; i<board.length; i++) {
+				if(this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i] && this.board[0][i] != Player.Unknown) {
+					result = true;
+					this.winner = currentPlayer;
+					return result;
+					
+				} else {
+					result = false;
+				}
+			}
+			
+			//Überprüfung ob diagonal gewonnen
+			if(this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2] && this.board[0][0] != Player.Unknown) {
+				result = true;
+				this.winner = currentPlayer;
+				return result;
+				
+			} else {
+				result = false;
+			}
+			
+			if(this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] != Player.Unknown) {
+				result = true;
+				this.winner = currentPlayer;
+				return result;
+				
+			} else {
+				result = false;
+			}
+			
+		} else if(this.winner == Player.Unknown) {
+			result = false;
 		} else {
-			return true;
+			result = true;
 		}
+		return result;
 	}
 
 	public boolean isDraw() {
-		boolean result = false;
+		boolean result = true;
 		
 		//Überprüfung ob Board voll, wenn Ergebnis true liefert ist Board voll
 		for(int i=0; i<this.board.length; i++){
 			for(int j=0; j<this.board[i].length; j++){
-				if(this.board[i][j] == Player.Unknown){
-					result = false;
+				if(this.board[i][j] == Player.X || this.board[i][j] == Player.O){
+					if(result != false) {
+						result = true;
+					}
+					
 				} else {
-					result = true;
+					
+					result = false;
 				}
 			}
 		}
 		
-		
-		//Überprüfung ob ein Spieler gewonnen hat -> falls true -> result = false;
-		
-		//Überprüfung ob Reihe gewonnen
-		for(int i=0; i<this.board.length; i++) {
-			
-			if(this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2] && this.board[i][0] != Player.Unknown) {
-				result = false;
-				
-			} else {
-				result = true;
-			}
-			
-		}
-		
-		//Überprüfung ob Spalte gewonnen
-		for(int i=0; i<board.length; i++) {
-			if(this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2] && this.board[i][0] != Player.Unknown) {
-				result = false;
-			} else {
-				result = true;
+		if(isGameWon() == true) {
+			return false;
+		} else {
+			if(result == true) {
+				this.winner = Player.Unknown;
 			}
 		}
 		
-		//Überprüfung ob diagonal gewonnen
-		if(this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2] && this.board[0][0] != Player.Unknown) {
-			result = false;
-		} else {
-			result = true;
-		}
 		
-		if(this.board[0][2] == this.board[1][1] && this.board[0][2] == this.board[2][0] && this.board[0][2] != Player.Unknown) {
-			result = false;
-		} else {
-			result = true;
-		}
+		
 		
 		
 		return result;
@@ -106,33 +138,23 @@ public class TicTacToe {
 	}
 
 	public boolean isGameOver() {
-		/*Itariert über Gitter und überprüft ob ein Spieler gewonnen hat, setzt Gewinner. Falls alle Felder besetzt aber kein Gewinner: winner = Unknown;
-		*return = exisitiert Gewinnner? ja/nein if(winner)== null -> false (isGameWon())
-		*
-		*
-		*
-		*
-		*
-		*
-		*
-		*/
-		
-		
-		
-		
-		
-		
+		System.out.println(isGameWon());
+		if(isGameWon() == true || isDraw() == true) {
+			return true;
+		} else {
+			return false;
+		}
 
-		return true;
 	}
 
 	public void play(int row, int col) {
 		
 		//Überprüfen ob noch gespielt werden kann
+		System.out.println(isGameOver());
 		if(isGameOver() == false) {
 			
 			//Überprüfen ob gewählte Koordinaten nicht Gittergröße übersteigen
-			if(row < 4 && col < 4){
+			if(row < ROWS && col < COLS){
 				
 				//spielen
 				//Überprüfen ob Platz bereits belegt
@@ -159,15 +181,13 @@ public class TicTacToe {
 					 * hat der aktuelle Spieler gewonnen. Dann muss der Spielstand aktualisiert werden (X hat ge-wonnen, O hat gewonnen, 
 					 * unentschieden).
 					 * 
-					 * 
-					 * 
-					 * 
-					 * 
-					 * 
-					 * 
-					 * 
 					 */
-					
+					this.currentPlayer = this.winner;
+					if(this.currentPlayer == Player.X) {
+						this.currentPlayer = Player.O;
+					} else {
+						this.currentPlayer = Player.X;
+					}
 					
 					
 					
@@ -205,4 +225,40 @@ public class TicTacToe {
 	        }
 	        return strBoard;
 	    }
+	 
+	 public void befuellen() {
+		 //für draw
+		 for(int i=0; i<3; i++) {
+			 for(int j=0; j<3; j++){
+					this.board[i][j] = Player.X;
+				}
+		 }
+		 this.board[0][0] = Player.O;
+		 this.board[1][1] = Player.O;
+		 this.board[1][2] = Player.O;
+		 this.board[0][1] = Player.O;
+		 this.board[2][0] = Player.O;
+		 
+		 this.board[0][0] = Player.Unknown;
+		 this.board[1][1] = Player.Unknown;
+
+	 }
+	 
+	 public void befuellen2() {
+		 for(int i=0; i<3; i++) {
+			 for(int j=0; j<3; j++){
+					this.board[i][j] = Player.X;
+				}
+		 }
+
+	 }
+	 public void befuellen3() {
+		 for(int i=0; i<3; i++) {
+			 for(int j=0; j<3; j++){
+					this.board[i][j] = Player.X;
+				}
+		 }
+		 this.board[0][0] = Player.Unknown;
+
+	 }
 }
